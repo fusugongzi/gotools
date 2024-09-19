3e414be5cac5b5e98a185d348208521eb67c3ba2
wangzhenzhong <15600804671@163.com>
2024-09-19 16:28:23 +0800

    feat: add ptr

--- conv/ptr.go
+++ conv/ptr.go
@@ -15,3 +15,31 @@ func Int64Ptr(v int64) *int64 {
 func StrPtr(v string) *string {
 	return &v
 }
+
+func PtrInt(v *int, dv int) int {
+	if v == nil {
+		return dv
+	}
+	return *v
+}
+
+func PtrInt32(v *int32, dv int32) int32 {
+	if v == nil {
+		return dv
+	}
+	return *v
+}
+
+func PtrInt64(v *int64, dv int64) int64 {
+	if v == nil {
+		return dv
+	}
+	return *v
+}
+
+func PtrStr(v *string, dv string) string {
+	if v == nil {
+		return dv
+	}
+	return *v
+}
